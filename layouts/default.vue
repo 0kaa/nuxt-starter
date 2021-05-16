8c957c53e66f4b0db9790c64457fbb989210d0a1
mahmoud sabry <thebestoka0@gmail.com>
2021-05-16 18:10:38 +0200

    starter project

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <Navbar />
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+import Navbar from '~/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <h1>Starter Project</h1>
+  </v-container>
+</template>
